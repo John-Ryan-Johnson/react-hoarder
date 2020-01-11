07b7007d5ce03c8e734f90e34c02d69aead0bbaf
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-01-10 20:45:00 -0600

    working on auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConnection from '../helpers/data/connection';
+import MyNavbar from '../components/shared/MyNavbar/MyNavbar';
 import './App.scss';
+import Auth from '../components/pages/Auth/Auth';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="btn btn-danger">Bootstrap Button</button>
-    </div>
-  );
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+firebaseConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    const { authed } = this.state;
+    return (
+      <div className="App">
+        <Router>
+          <MyNavbar authed={authed}/>
+          <Switch>
+            <PublicRoute path="/auth" exact component={Auth} authed={authed} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/pages/Auth/Auth.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Auth.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+
+class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  render() {
+    return (
+      <div className="Auth">
+        <h1>Auth Page</h1>
+        <button className="btn btn-danger" onClick={this.loginClickEvent}>Log In With Google</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
